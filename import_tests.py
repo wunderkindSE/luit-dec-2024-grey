046d338062e0699159448876856c36ca02a25589
Samuel Ekpe <31948354+wunderkindSE@users.noreply.github.com>
2024-12-19 13:55:23 -0600

    Create import_tests.py

---
+++ import_tests.py
@@ -0,0 +1,13 @@
+import hello_world
+
+# Import random for generating random numbers
+import random 
+
+# Randint is the function that returns a random integer
+print("Display a random number between 1 and 1000:", random.randint(1, 1000))
+
+# Import math for mathematical operations
+import math
+
+# Calculating the square root of a number
+print("Square root of 25:", math.sqrt(25))
