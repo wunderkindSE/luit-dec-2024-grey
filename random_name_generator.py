a72a3ec5bc4f8eae9385971e10163738a176c6a9
Samuel Ekpe <31948354+wunderkindSE@users.noreply.github.com>
2024-12-19 13:40:33 -0600

    Create random_name_generator.py

---
+++ random_name_generator.py
@@ -0,0 +1,8 @@
+import string #allow chars
+import random #library to generate random numbers
+
+department = ['Marketing', 'Accounting', 'FinOps']
+
+input('What department are you in? ')
+
+input('How many EC2 instances do you want? ')
