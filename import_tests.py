2acfc418a42bd78f3b43573cf188fd1bcd653b9f
Samuel Ekpe <31948354+wunderkindSE@users.noreply.github.com>
2024-12-19 14:06:39 -0600

    Update import_tests.py

---
+++ import_tests.py
@@ -0,0 +1,18 @@
+import hello_world
+import random 
+
+# Randint is the function that returns a random integer
+print("Display a random number between 1 and 1000:", random.randint(1, 1000))
+
+# Import math for mathematical operations
+import math
+
+# Calculating the square root of a number
+print("Square root of 25:", math.sqrt(25))
+
+# Import os to interact with the operating system
+import os
+
+# Print the current working directory
+print("Current working directory:", os.getcwd())
+
