7a93b6320d31e4b9414748ed9902dbad947c231e
Samuel Ekpe <31948354+wunderkindSE@users.noreply.github.com>
2025-01-08 12:19:26 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,4 +12,18 @@ def get_s3_client() -> boto3.client:
     return boto3.client('s3')
 
 #Describes EC2 instances and returns a list of instances
-#def describe_instances(client: boto3.client) -> list:
+'''
+def describe_instances(client: boto3.client) -> list:
+    """"""
+    Args:
+        client (boto3.client): The EC2 client used to describe instances.
+    
+    Returns:
+        list: A list of instances.
+    """"""
+    response = client.describe_instances() # 
+    instances = []
+    for reservation in response['Reservations']: # 
+        instances.extend(reservation['Instances']) # 
+        return instances
+    '''
